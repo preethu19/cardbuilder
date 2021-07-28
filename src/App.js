b36810c0be812df67a2555a3da1559aaef15d2a3
unknown <preethamkumar2000@gmail.com>
2021-07-28 11:05:31 +0530

    added undo redo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { v4 as uuid } from 'uuid';
 import { Html } from "react-konva-utils";
 
 
-const Rectangle = ({ shapeProps, isSelected, onSelect, onMove, onChange }) => {
+const Rectangle = ({ shapeProps, isSelected, onSelect, onMove, onChange, useTool }) => {
   const shapeRef = React.useRef();
   const trRef = React.useRef();
 
@@ -24,7 +24,7 @@ const Rectangle = ({ shapeProps, isSelected, onSelect, onMove, onChange }) => {
         onDblTap={onSelect}
         ref={shapeRef}
         {...shapeProps}
-        // draggable
+        draggable={!useTool && isSelected}
         // onDragStart={onMove}
         // onTouchStart={onMove}
         onDragEnd={(e) => {
@@ -75,7 +75,7 @@ const Rectangle = ({ shapeProps, isSelected, onSelect, onMove, onChange }) => {
 
 
 
-const Circles = ({ shapeProps, isSelected, onSelect, onMove, onChange }) => {
+const Circles = ({ shapeProps, isSelected, onSelect, onMove, onChange, useTool }) => {
   const shapeRef = React.useRef();
   const trRef = React.useRef();
 
@@ -94,7 +94,7 @@ const Circles = ({ shapeProps, isSelected, onSelect, onMove, onChange }) => {
         onDblTap={onSelect}
         ref={shapeRef}
         {...shapeProps}
-        // draggable
+        draggable={!useTool && isSelected}
         // onDragStart={onMove}
         // onTouchStart={onMove}
         onDragEnd={(e) => {
@@ -143,7 +143,7 @@ const Circles = ({ shapeProps, isSelected, onSelect, onMove, onChange }) => {
   );
 };
 
-const Texts = ({ shapeProps, isSelected, onSelect, onMove, onChange }) => {
+const Texts = ({ shapeProps, isSelected, onSelect, onMove, onChange, useTool }) => {
   const shapeRef = React.useRef();
   const trRef = React.useRef();
   
@@ -167,7 +167,7 @@ const Texts = ({ shapeProps, isSelected, onSelect, onMove, onChange }) => {
         onDblTap={onSelect}
         ref={shapeRef}
         {...shapeProps}
-        // draggable
+        draggable={!useTool && isSelected}
         // onDragStart={onMove}
         // onTouchStart={onMove}
         onDragEnd={(e) => {
@@ -218,7 +218,98 @@ const Texts = ({ shapeProps, isSelected, onSelect, onMove, onChange }) => {
 };
 
 
+const Lines = ({ shapeProps, isSelected, onSelect, onMove, onChange, points, tool, useTool }) => {
+  const shapeRef = React.useRef();
+  const trRef = React.useRef();
+  
+  React.useEffect(() => {
+    if (isSelected) {
+      // we need to attach transformer manually
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
 
+  
+  
+ 
+
+  return (
+    <React.Fragment>
+    
+      <Line
+       
+        onDblClick={onSelect}
+        onDblTap={onSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        points={points}
+        stroke="#df4b26"
+        strokeWidth={5}
+        tension={0.5}
+        lineCap="round"
+        draggable={!useTool && isSelected}
+        globalCompositeOperation={
+          tool === 'eraser' ? 'destination-out' : 'source-over'
+        }
+        // onDragStart={onMove}
+        // onTouchStart={onMove}
+        onDragEnd={(e) => {
+          
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y()
+          });
+        }}
+        onTransformEnd={(e) => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // but in the store we have only width and height
+          // to match the data better we will reset scale on transform end
+          const node = shapeRef.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+          
+          // we will reset it back
+          node.scaleX(1);
+          node.scaleY(1);
+          let newPoints = [];
+          
+          for (let i=0; i<shapeProps.points.length; i=i+2){
+            newPoints = newPoints.concat([shapeProps.points[i]*scaleX, shapeProps.points[i+1]*scaleY]) 
+            
+          }
+          onChange({
+            ...shapeProps,
+            // x: node.x(),
+            // y: node.y(),
+            // set minimal value
+            points: newPoints
+            // width: Math.max(5, node.width() * scaleX),
+            // height: Math.max(node.height() * scaleY),
+          });
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          boundBoxFunc={(oldBox, newBox) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
+
+let history = [{shapes: [], lines:[]}]
+let historyStep = 0;
 
 
 export default function App() {
@@ -232,54 +323,90 @@ export default function App() {
   const [lines, setLines] = React.useState([]);
   const isDrawing = React.useRef(false);
   const [useTool, setUseTool] = useState(false)
+  const [handleDraw, setHandleDraw] = useState(false)
+  
+
+  const handleUndo = () => {
+    console.log('undo')
+    if (historyStep === 0) {
+      return;
+    }
+    historyStep -= 1;
+    const previous = history[historyStep];
+    setLines(previous.lines)
+    setShapes(previous.shapes)
+  }
+
+  const handleRedo = () => {
+    console.log('redo')
+    if (historyStep === history.length - 1) {
+      return;
+    }
+    historyStep += 1;
+    const next = history[historyStep];
+    setLines(next.lines)
+    setShapes(next.shapes)
+  }
 
   const handleMouseDown = (e) => {
     if(useTool){
       isDrawing.current = true;
-      const pos = e.target.getStage().getPointerPosition();
-      setShapes([...shapes, { tool, points: [pos.x, pos.y], name: "line", index: lines.length }]);
-      setLines([...lines, { tool, points: [pos.x, pos.y], name: "line" }]);
+      // const transform = e.target.getAbsoluteTransform().copy()
+      // transform.invert()
+      const stage = e.target.getStage()
+      let pos = stage.getPointerPosition();
+      // pos = transform.point(pos)
+      setLines([...lines, { tool, points: [pos.x, pos.y], name: "line", x: 0, y: 0, id: uuid() }]);
+      setShapes([...shapes, { tool, points: [pos.x, pos.y], name: "line", index: lines.length , x: e.target.x(), y: e.target.y(), id: uuid() }]);
       
+      setHandleDraw(true)
     }
     checkDeselect(e)
   };
 
   const handleMouseMove = (e) => {
     if(useTool){
-      // no drawing - skipping
-    if (!isDrawing.current) {
-      return;
-    }
-    const stage = e.target.getStage();
-    const point = stage.getPointerPosition();
-    let lastLine = lines[lines.length - 1];
-    // add point
-    lastLine.points = lastLine.points.concat([point.x, point.y]);
+      if(handleDraw){
+        // no drawing - skipping
+        if (!isDrawing.current) {
+          return;
+        }
+        const stage = e.target.getStage();
+        const point = stage.getPointerPosition();
+        let lastLine = lines[lines.length - 1];
+        // add point
+        lastLine.points = lastLine.points.concat([point.x, point.y]);
 
-    // replace last
-    lines.splice(lines.length - 1, 1, lastLine);
-    setLines(lines.concat());
+        // replace last
+        lines.splice(lines.length - 1, 1, lastLine);
+        setLines(lines.concat());
 
-    // let lastLine = ''
-    // for(var i=shapes.length-1; i > 0;i--){
-    //   if(shapes[i].name=='line'){
-    //     lastLine = shapes[shapes.length - 1];
-    //   }
-    // }
-    
-    // // add point
-    // lastLine.points = lastLine.points.concat([point.x, point.y]);
+        // let lastLine = ''
+        // for(var i=shapes.length-1; i > 0;i--){
+        //   if(shapes[i].name=='line'){
+        //     lastLine = shapes[shapes.length - 1];
+        //   }
+        // }
 
-    // // replace last
-    // shapes.splice(shapes.length - 1, 1, lastLine);
-    // setShapes(shapes.concat());
-    }
+        // // add point
+        // lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+        // // replace last
+        // shapes.splice(shapes.length - 1, 1, lastLine);
+        // setShapes(shapes.concat());
+        }
+      }
+      
     
   };
 
   const handleMouseUp = () => {
     if(useTool){
       isDrawing.current = false;
+      setHandleDraw(false)
+      history = history.slice(0, historyStep + 1);
+      history = history.concat([{shapes: shapes, lines:lines}]);
+      historyStep += 1;
     }
     
   };
@@ -304,18 +431,28 @@ export default function App() {
   setShapes((prevShape)=>(
     [...prevShape, item]
   ))
+  history = history.slice(0, historyStep + 1);
+  history = history.concat([{shapes: [...shapes, item], lines:lines}]);
+  historyStep += 1;
 }
 
   const onEdit = (e) => {
+    let pos = {}
     setEditShape((shape)=> ({...shape, text:e.target.value}))
     setShapes(shapes.map((elem)=>{
       if(elem.id==selectedId){
-        return {...elem, text:e.target.value}
+        pos = {...elem, text:e.target.value}
+        return pos
       }
       return elem
     })
     )
+    history = history.slice(0, historyStep + 1);
+    history = history.concat([{shapes: [...shapes, pos], lines:lines}]);
+    historyStep += 1;
   }
+
+  
   return (
     <>
     <Stage 
@@ -352,7 +489,11 @@ export default function App() {
                 const rects = shapes.slice();
                 rects[i] = newAttrs;
                 setShapes(rects);
+                history = history.slice(0, historyStep + 1);
+                history = history.concat([{shapes: rects, lines:lines}]);
+                historyStep += 1;
               }}
+              useTool={useTool}
             />
             )
         }
@@ -373,7 +514,11 @@ export default function App() {
               const rects = shapes.slice();
               rects[i] = newAttrs;
               setShapes(rects);
+              history = history.slice(0, historyStep + 1);
+              history = history.concat([{shapes: rects, lines:lines}]);
+              historyStep += 1;
             }}
+            useTool={useTool}
           />
         )
         }
@@ -396,43 +541,79 @@ export default function App() {
                 const rects = shapes.slice();
                 rects[i] = newAttrs;
                 setShapes(rects);
+                history = history.slice(0, historyStep + 1);
+                history = history.concat([{shapes: rects, lines:lines}]);
+                historyStep += 1;
               }}
+              useTool={useTool}
             />
           )
         }
-        {/* else if(eachShape.name=="line"){
-          
+        else if(eachShape.name=="line"){
+         
           return(
-            <Line
+            <Lines
               key={i}
               points={lines[eachShape.index].points}
-              stroke="#df4b26"
-              strokeWidth={5}
-              tension={0.5}
-              lineCap="round"
-              globalCompositeOperation={
-                lines[eachShape.index].tool === 'eraser' ? 'destination-out' : 'source-over'
-              }
+              tool={lines[eachShape.index].tool}
+              shapeProps={lines[eachShape.index]}
+              isSelected={lines[eachShape.index].id === selectedId}
+              onSelect={() => {
+                setUseTool(false)
+                onDelete(eachShape);
+                selectShape(lines[eachShape.index].id);
+                
+              }}
+              onMove={()=>onDelete(eachShape)}
+              onChange={(newAttrs) => {
+                const rects = lines.slice();
+                rects[eachShape.index] = newAttrs;
+                setLines(rects);
+                history = history.slice(0, historyStep + 1);
+                history = history.concat([{shapes: shapes, lines:rects}]);
+                historyStep += 1;
+              }}
+              useTool={useTool}
             />
           )
          
-        } */}
+        }
       })
       }
-      
-      {lines.map((line, i) => (
-            <Line
-              key={i}
-              points={line.points}
+      {/* <Line
+             x={200}
+             y={300}
+              points={[100, 100, 200, 200]}
               stroke="#df4b26"
               strokeWidth={5}
               tension={0.5}
               lineCap="round"
               globalCompositeOperation={
-                line.tool === 'eraser' ? 'destination-out' : 'source-over'
+                'source-over'
               }
+            /> */}
+      {/* {lines.map((line, i) => (
+            <Lines
+              key={i}
+              points={line.points}
+              tool={line.tool}
+              shapeProps={line}
+              isSelected={line.id === selectedId}
+              onSelect={() => {
+                setUseTool(false)
+                onDelete(line);
+                selectShape(line.id);
+                
+              }}
+              onMove={()=>onDelete(line)}
+              onChange={(newAttrs) => {
+                const rects = lines.slice();
+                rects[i] = newAttrs;
+                setLines(rects);
+              }}
+            
             />
-          ))}
+          ))} */}
 
         <Circle
           name="draggableCircle1"
@@ -448,25 +629,32 @@ export default function App() {
           y={50}
           radius={25}
           stroke="black"
-          draggable
+          draggable={!useTool}
           id="circle2"
-          onClick={()=>setUseTool(false)}
-          onTap={()=>setUseTool(false)}
-          onDragStart={()=>setUseTool(false)}
+          // onClick={()=>setUseTool(false)}
+          // onTap={()=>setUseTool(false)}
+          // onDragStart={()=>setUseTool(false)}
           onDragEnd={(e) => {
+            
+            
             // push new circle to view
             // note that we must push circle first before returning draggable circle
             // because e.target.x returns draggable circle's positions
+            const pos = { x: e.target.x(), y: e.target.y(), radius: 25, stroke:"black", id: uuid(), fill: "red", name:"circle" }
+            
             setShapes((prevCircles) => [
               ...prevCircles,
-              { x: e.target.x(), y: e.target.y(), radius: 25, stroke:"black", id: uuid(), fill: "red", name:"circle" }
+              pos
             ]);
-
+            console.log(shapes)
             // return draggable circle to original position
             // notice the dot (.) before "draggableCircle"
             var stage = stageRef.current;
             var draggableCircle = stage.findOne(".draggableCircle");
             draggableCircle.position({ x: 50, y: 50 });
+            history = history.slice(0, historyStep + 1);
+            history = history.concat([{shapes: [...shapes, pos], lines:lines}]);
+            historyStep += 1;
           }}
         />
 
@@ -486,17 +674,20 @@ export default function App() {
         height= {50}
         stroke="black"
         id= 'rect1'
-        draggable
-        onClick={()=>setUseTool(false)}
-        onTap={()=>setUseTool(false)}
-        onDragStart={()=>setUseTool(false)}
+        draggable={!useTool}
+        
+        // onClick={()=>setUseTool(false)}
+        // onTap={()=>setUseTool(false)}
+        // onDragStart={()=>setUseTool(false)}
         onDragEnd={(e) => {
+          
             // push new circle to view
             // note that we must push circle first before returning draggable circle
             // because e.target.x returns draggable circle's positions
+            const pos = { x: e.target.x(), y: e.target.y(), stroke:"black", width: 50, height: 50, id: uuid(), fill: "green", name:"rectangle"}
             setShapes((prevRectangles) => [
               ...prevRectangles,
-              { x: e.target.x(), y: e.target.y(), stroke:"black", width: 50, height: 50, id: uuid(), fill: "green", name:"rectangle"}
+              pos
             ]);
 
             // return draggable circle to original position
@@ -504,6 +695,9 @@ export default function App() {
             var stage = stageRef.current;
             var draggableRectangle = stage.findOne(".draggableRect");
             draggableRectangle.position({ x: 25, y: 80 });
+            history = history.slice(0, historyStep + 1);
+            history = history.concat([{shapes: [...shapes, pos], lines:lines}]);
+            historyStep += 1;
           }}
       />
       <Text
@@ -522,18 +716,18 @@ export default function App() {
           fontSize={50}
           width={40}
           height={100}
-          draggable
-          onClick={()=>setUseTool(false)}
-          onTap={()=>setUseTool(false)}
-          onDragStart={()=>setUseTool(false)}
+          draggable={!useTool}
+          // onClick={()=>setUseTool(false)}
+          // onTap={()=>setUseTool(false)}
+          // onDragStart={()=>setUseTool(false)}
           onDragEnd={(e) => {
             // push new circle to view
             // note that we must push circle first before returning draggable circle
             // because e.target.x returns draggable circle's positions
-            
+            const pos = { x: e.target.x(), y: e.target.y(), id: uuid(), text: "T", width: 40, textEditVisible:true, height:100, fontSize: 50, name:"text"}
             setShapes((prevTexts) => [
               ...prevTexts,
-              { x: e.target.x(), y: e.target.y(), id: uuid(), text: "T", width: 40, textEditVisible:true, height:100, fontSize: 50, name:"text"}
+              pos
             ]);
 
             // return draggable circle to original position
@@ -541,12 +735,18 @@ export default function App() {
             var stage = stageRef.current;
             var draggableText = stage.findOne(".draggableText");
             draggableText.position({ x: 35, y: 140 });
+            history = history.slice(0, historyStep + 1);
+            history = history.concat([{shapes: [...shapes, pos], lines:lines}]);
+            historyStep += 1;
           }}
       />
      <Html>
         <div>
+        <input type="button" value="Undo" onClick={handleUndo}/>
+        <input type="button" value="Redo" onClick={handleRedo} />
+        <br />
         <input type="button" value="Pencil" style={tool=='pen' && useTool?{backgroundColor: "aqua"}:null} onClick={()=>{setTool('pen'); setUseTool(!useTool)}}/>
-        <input type="button" value="Eraser" style={tool=='eraser' && useTool?{backgroundColor: "aqua"}:null} onClick={()=>{setTool('eraser'); setUseTool(!useTool)}} />
+        {/* <input type="button" value="Eraser" style={tool=='eraser' && useTool?{backgroundColor: "aqua"}:null} onClick={()=>{setTool('eraser'); setUseTool(!useTool)}} /> */}
         </div>
         {textEdit?
           <textarea value={editShape.text} onChange={onEdit}></textarea>
